72d8d326eb29d3e6926e168794d04a75e2083f02
ShompiFlen <shompire02@gmail.com>
2023-06-30 22:05:27 -0400

    find () se cuelga al no poder establecer conexión con la bd

--- src/controllers/postulacionController.js
+++ src/controllers/postulacionController.js
@@ -1,29 +1,29 @@
 // Importa el modelo de datos 'Postulante'
-const Postulante = require('../models/postulacionModel.js');
+import { Postulante } from '../models/postulacionModel.js';
 
-exports.getPostulantes = async (req, res) => {
-  try {
-    const postulantes = await Postulante.find();
-    res.status(200).json(postulantes);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
+export async function getPostulantes(req, res) {
+	try {
+		// const postulantes = await Postulante.find();
+		res.status(200).json({ message: "Peticion recibida con exito!" });
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+}
 
-exports.createPostulante = async (req, res) => {
-  try {
-    const postulante = new Postulante({
-      nombre: req.body.nombre,
-      email: req.body.email,
-      edad: req.body.edad,
-      telefono: req.body.telefono,
-      direccion: req.body.direccion,
-      AntecedenteSalud: req.body.AntecedenteSalud,
-    });
+export async function createPostulante(req, res) {
+	try {
+		const postulante = new Postulante({
+			nombre: req.body.nombre,
+			email: req.body.email,
+			edad: req.body.edad,
+			telefono: req.body.telefono,
+			direccion: req.body.direccion,
+			AntecedenteSalud: req.body.AntecedenteSalud,
+		});
 
-    const nuevoPostulante = await postulante.save();
-    res.status(201).json(nuevoPostulante);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-};
+		const nuevoPostulante = await postulante.save();
+		res.status(201).json(nuevoPostulante);
+	} catch (error) {
+		res.status(400).json({ message: error.message });
+	}
+}

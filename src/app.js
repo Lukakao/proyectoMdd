8994da1b0ac1ad399c5c09d1c9e9808d7936a14f
ShompiFlen <shompire02@gmail.com>
2023-06-30 22:02:57 -0400

    añadidas otras rutas

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
 import express from 'express';
 import morgan from 'morgan';
 
-import authRoutes from './routes/auth.routes.js'
-import cursosRoutes from "./routes/cursos.routes.js"
+import authRoutes from './routes/auth.routes.js';
+import cursosRoutes from "./routes/cursos.routes.js";
+import indexRoutes from "./routes/index.routes.js";
+import postulacionRoutes from "./routes/postulacion.routes.js";
 
 const app = express();
+app.use(morgan("dev"));
+app.use(express.json());
 
-app.use(morgan("dev"))
-app.use(express.json())
-
-app.use("/api", authRoutes)
-app.use("/cursos", cursosRoutes)
+app.use("/api", authRoutes);
+app.use("/cursos", cursosRoutes);
+app.use("/postulante", indexRoutes);
 
 export default app;

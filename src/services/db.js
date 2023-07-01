165401b9b0790aa66547e32167c1bae7933c4a6d
ShompiFlen <shompire02@gmail.com>
2023-06-30 22:06:07 -0400

    indentación y cambios de CJS a ESM

--- src/services/db.js
+++ src/services/db.js
@@ -1,23 +1,23 @@
 import mongoose from "mongoose";
-import {configEnv} from "./configEnv.js";
+import { configEnv } from "./configEnv.js";
 const env = configEnv();
 
 export const setUpServer = (server) => {
-  try {
-    mongoose
-            .connect(env.URI, {
-              useNewUrlParser: true,
-              useUnifiedTopology: true,
-            })
-            .then(() =>
-                    server.listen(env.PORT, () =>
-                            console.log(`Server Running on Port: http://localhost:${env.PORT}`)
-                    )
-            )
-            .catch((error) => console.log(`${error} did not connect`));
-  } catch (error) {
-    console.error(error);
-  }
+	try {
+		mongoose
+			.connect(env.URI, {
+				useNewUrlParser: true,
+				useUnifiedTopology: true,
+			})
+			.then(() =>
+				server.listen(env.PORT, () =>
+					console.log(`Server Running on Port: http://localhost:${env.PORT}`)
+				)
+			)
+			.catch((error) => console.log(`${error} did not connect`));
+	} catch (error) {
+		console.error(error);
+	}
 };
 
-module.exports = { setUpServer };
+export { setUpServer };

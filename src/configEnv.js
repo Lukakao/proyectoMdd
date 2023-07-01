20169e7b037d52ba2048959fd36485511453e080
ShompiFlen <shompire02@gmail.com>
2023-06-30 22:04:14 -0400

    Simplificada la funcion para obtener los datos del .env

--- src/configEnv.js
+++ src/configEnv.js
@@ -1,17 +1,6 @@
-//Importa el modulo 'path' para obtener la ruta absoluta del archivo .env
-import { resolve } from 'path';
-
 // Obtiene la ruta absoluta del archivo .env
-const configEnv = () => {
-  const envFilePath = resolve(__dirname, `.env`);
-  // Carga las variables de entorno desde el archivo .env
-  require('dotenv').config({ path: envFilePath });
-    // Retorna un objeto con las variables de entorno
-  return {
-    PORT: process.env.PORT,
-    HOST: process.env.HOST,
-    DB_URL: process.env.DB_URL,
-  };
+export const configEnv = {
+	PORT: process.env.PORT,
+	HOST: process.env.HOST,
+	DB_URL: process.env.DB_URL,
 };
-
-export default { configEnv };

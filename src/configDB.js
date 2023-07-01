1d44f82a46313c6d46ce8b6b6382cb0ddd82774e
ShompiFlen <shompire02@gmail.com>
2023-06-30 22:03:46 -0400

    funcion para crear una conexión con la base de datos

--- src/configDB.js
+++ src/configDB.js
@@ -2,18 +2,24 @@
 import mongoose from 'mongoose';
 
 // Agregamos la configuración de las variables de entorno
-import {configEnv} from "./configEnv.js"
+import { configEnv } from "./configEnv.js"
+
+console.log("configs", configEnv);
+
 
 // Obtiene las variables de entorno
 const { DB_URL } = configEnv();
 
 // Opciones de configuración para la conexión a la base de datos
 const options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
 };
 
-// Conecta a la base de datos
-mongoose.connect(DB_URL, options)
-    .then(() => console.log('Conectado a la base de datos'))
-    .catch(err => console.log(err));
+
+export const connectDb = async () => {
+	// Conecta a la base de datos
+	const connection = await mongoose.connect(DB_URL, options)
+
+	return connection
+}
